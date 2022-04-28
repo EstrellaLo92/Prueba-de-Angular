c7c1652b3a7437974271ccc0485821449635d94d
DanielMoraTorres <danielalejandrol.moratorres@gmail.com>
2022-04-28 18:24:30 -0400

    scss2 commit

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,24 +1,26 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { AuthRoutingModule } from './auth-routing.module';
-import { LoginComponent } from './pages/login/login.component';
-import { RegistroComponent } from './pages/registro/registro.component';
-import { MatIconModule } from '@angular/material/icon';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { SharedModule } from '../shared/shared.module';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatDividerModule } from '@angular/material/divider';
-import { ReactiveFormsModule } from '@angular/forms';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { MatListModule } from '@angular/material/list';
+import { AuthRoutingModule } from "./auth-routing.module";
+import { LoginComponent } from "./pages/login/login.component";
+import { RegistroComponent } from "./pages/registro/registro.component";
+import { MatIconModule } from "@angular/material/icon";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { SharedModule } from "../shared/shared.module";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatDividerModule } from "@angular/material/divider";
+import { ReactiveFormsModule } from "@angular/forms";
+import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { MatListModule } from "@angular/material/list";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 @NgModule({
   declarations: [LoginComponent, RegistroComponent, DashboardComponent],
   imports: [
     ReactiveFormsModule,
+    MatToolbarModule,
     MatDividerModule,
     MatListModule,
     MatCardModule,
